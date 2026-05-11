b852908f0f78abd64dc8eda596ad677cbe53da0a
Abdussomad <abdussomad8720@gmail.com>
2026-05-11 13:22:40 +0100

    change company name to greenpeg

--- dashboard/src/components/Header.jsx
+++ dashboard/src/components/Header.jsx
@@ -1,18 +1,24 @@
 import useSensorStore from "../store/useSensorStore";
- 
+
 export default function Header() {
   const connected = useSensorStore((s) => s.connected);
-  const data      = useSensorStore((s) => s.sensorData);
+  const data = useSensorStore((s) => s.sensorData);
   return (
-    <div className="flex items-center justify-between
-                    bg-gray-900 border-b border-green-800 px-6 py-4">
+    <div
+      className="flex items-center justify-between
+                    bg-gray-900 border-b border-green-800 px-6 py-4"
+    >
       <div>
-        <h1 className="text-xl font-bold text-white">GRENPEG IIoT Monitoring Dashboard</h1>
+        <h1 className="text-xl font-bold text-white">
+          GREENPEG IIoT Monitoring Dashboard
+        </h1>
         <p className="text-xs text-gray-400">Oil and Gas industry</p>
       </div>
       <div className="flex items-center gap-3">
-        <span className={`px-3 py-1 rounded-full text-xs font-bold
-          ${connected ? "bg-green-600" : "bg-red-600"} text-white`}>
+        <span
+          className={`px-3 py-1 rounded-full text-xs font-bold
+          ${connected ? "bg-green-600" : "bg-red-600"} text-white`}
+        >
           {connected ? "● LIVE" : "● OFFLINE"}
         </span>
         {data && (
