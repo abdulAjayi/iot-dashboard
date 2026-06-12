560024aab2fc84a049cf76f375458a240b400781
abdulAjayi <abdussomad8720@gmailcom>
2026-06-12 17:21:47 +0100

    fix: ngrok browser warning issue

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,11 @@ app.use(
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "ngrok-skip-browser-warning",
+    ],
     credentials: true,
   }),
 );
