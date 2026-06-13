f6ebb1c6dd21cc8ef4b30b2f790ca3469362b6ac
abdulAjayi <abdussomad8720@gmailcom>
2026-06-13 17:47:05 +0100

    fix cors error on refresh

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -19,6 +19,9 @@ function App() {
             headers: {
               Authorization: `Bearer ${token}`,
             },
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
           },
         );
         console.log(res);
