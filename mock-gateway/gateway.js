b856e53a3ecec61957a5144720a5e29b9969433e
abdulAjayi <abdussomad8720@gmailcom>
2026-06-16 09:54:20 +0100

    fix gateway interval error and dashboard header ui

--- mock-gateway/gateway.js
+++ mock-gateway/gateway.js
@@ -3,13 +3,17 @@ import { baselines, wells } from "./wellConfig.js";
 import { fluctuate } from "./sensorUtils.js";
 import { generateSensorData } from "./sensorUtils.js";
 // const WS_URL = "wss://iot-dashboard-ve7n.onrender.com?type=gateway";
-const WS_URL = "wss://backslid-deflate-hangnail.ngrok-free.dev?type=gateway";
-// const WS_URL = "ws://localhost:3000?type=gateway";
+// const WS_URL = "wss://backslid-deflate-hangnail.ngrok-free.dev?type=gateway";
+const WS_URL = "ws://localhost:3000?type=gateway";
+let interValid = null
 function connect() {
   const ws = new WebSocket(WS_URL);
   ws.on("open", () => {
     console.log("gateway connected successfully");
-    setInterval(() => {
+
+    if(interValid) clearInterval(interValid)
+
+    interValid = setInterval(() => {
       wells.forEach((well) => {
         const sensorData = generateSensorData(well.id);
         const payload = { type: "sensor_data", wellId: well.id, ...sensorData };
