997382336f1cc2f6ca412b16fce3d15adad72fe8
abdulAjayi <abdussomad8720@gmailcom>
2026-06-16 15:10:52 +0100

    fix dashboard url connection

--- dashboard/src/hooks/useSocket.js
+++ dashboard/src/hooks/useSocket.js
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react";
 import useWellStore from "../store/useWellStore";
 import useAuthStore from "../store/useAuthStore";
 // const ws_url = "wss://backslid-deflate-hangnail.ngrok-free.dev?type=dashboard";
-const ws_url_render = "wss://iot-dashboard-ve7n.onrender.com?type=dashboard";
+const ws_url = "wss://iot-dashboard-ve7n.onrender.com?type=dashboard";
 // const ws_url = "ws://localhost:3000?type=dashboard";
 export function useSocket() {
   const ws = useRef(null);
